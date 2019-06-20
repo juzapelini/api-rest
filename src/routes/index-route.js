a37b2f4a9d4f70410ee3fb98d5541d42f55dd693
Juliano Zapelini <juliano.batista@bradesco.com.br>
2019-06-20 14:36:50 -0300

    v1.0.0

--- src/routes/index-route.js
+++ src/routes/index-route.js
@@ -6,7 +6,7 @@ const router = express.Router();
 router.get('/', (req, res, next) => {
     res.status(200).send({
         title: "Node store API",
-        version: "0.0.2"
+        version: "1.0.0"
     })
 });
 
